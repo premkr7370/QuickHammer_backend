55870c8ea598a0f675f8c2565d17ac1c80d20e70
Prem Kumar <118460551+premkr7370@users.noreply.github.com>
2025-05-07 02:56:29 +0530

    Update bidRoutes.js

--- router/bidRoutes.js
+++ router/bidRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { placeBid } from "../controllers/bidController.js";
 import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
-import { checkAuctionEndTime } from "../middlewares/CheckAuctionEndTime.js";
+import { checkAuctionEndTime } from "../middlewares/checkAuctionEndTime.js";
+
 
 
 const router = express.Router();
