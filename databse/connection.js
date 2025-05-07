3a90c1e66b1d31b9195e96a0412702438d66585c
Prem Kumar <118460551+premkr7370@users.noreply.github.com>
2025-05-07 19:11:42 +0530

    Update connection.js

--- databse/connection.js
+++ databse/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "Prime_Bid",
+      dbName: "quickhammer",
     })
     .then(() => {
       console.log("Connected to database.");
